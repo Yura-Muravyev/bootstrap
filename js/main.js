06dac7f241a0c267a17255a485d7554f8ab9a28e
Yuriy Muravyev <yura.muravyev@yandex.ru>
2022-04-27 15:57:24 +0300

    v3

--- js/main.js
+++ js/main.js
@@ -1,38 +1,36 @@
 const studensData = [];
 
+function addUserTable() {
+  let tbody = document.querySelector('tbody');
 
-function setData (name, middleName, sureName) {
-  let itemObj = {};
-      itemObj.name = name,
-      itemObj.middleName = middleName,
-      itemObj.sureName = sureName,
-  studensData.push(itemObj);
+  for(var i = 0; i < studensData.length; i++) {
+    let tr = document.createElement('tr');
+    tr.innerHTML =
+      '<td>' + studensData[i].sureName + ' ' + studensData[i].name + ' ' + studensData[i].middleName +'</td>' + 
+      '<td>' + studensData[i].faculty + '</td>' + 
+      '<td>' + studensData[i].birthday + '</td>' + 
+      '<td>' + studensData[i].startTraining + '</td>'; 
+    tbody.appendChild(tr);
+  }
+
 }
 
-function serializeForm(formNode) {
-  const { elements } = formNode;
-  const data = Array.from(elements)
-    .filter((item) => !!item.name);
-  return data;
-}
 
-function validation(event) {
+function setForm() {
   event.preventDefault();
-
-  const elemntFrom = serializeForm(document.querySelector('#form'))
-  elemntFrom.forEach(item => {
-    if (item.value.trim() === '') {
-      item.style.border = '2px solid red'
-      
-    } else {
-      
-      setData(elemntFrom[0].value, elemntFrom[1].value, elemntFrom[2].value,) 
-    }
-    
-  });
-  console.log(studensData);
-};
-
+  const FORM = document.forms.form;
+  let user = {
+    sureName: FORM.elements.sureName.value,
+    name: FORM.elements.name.value,
+    middleName: FORM.elements.middleName.value,
+    faculty: FORM.elements.faculty.value,
+    birthday: new Date(FORM.elements.birthday.value),
+    startTraining: FORM.elements.startTraining.value,
+  }
+  
+  studensData.push(user);
+  addUserTable();
+}
 
 const submit = document.querySelector('#form');
-submit.addEventListener('submit', validation);
+submit.addEventListener('submit', setForm);
