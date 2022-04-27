12bfb995e20ae0e28463fd4e5b629da2e382a659
yura muravyev <yura.muravyev@yandex.ru>
2022-04-27 22:12:41 +0300

    v3.2

--- js/main.js
+++ js/main.js
@@ -1,28 +1,22 @@
 const studensData = [];
 
-
-
-function timeСalculation(birthday) {
-  let
-}
-
 function addUserTable() {
+  event.preventDefault();
+  const user = setForm();
   let tbody = document.querySelector('tbody');
-
-  for(var i = 0; i < studensData.length; i++) {
-    let tr = document.createElement('tr');
-    tr.innerHTML =
-      '<td>' + studensData[i].sureName + ' ' + studensData[i].name + ' ' + studensData[i].middleName +'</td>' + 
-      '<td>' + studensData[i].faculty + '</td>' + 
-      '<td>' + +studensData[i].birthday.toLocaleDateString() + '</td>' + 
-      '<td>' + studensData[i].startTraining + '</td>'; 
-    tbody.appendChild(tr);
-  }
+  let tr = document.createElement('tr');
+  tr.innerHTML =
+      '<td>' + user.sureName + ' ' + user.name + ' ' + user.middleName +'</td>' + 
+      '<td>' + user.faculty + '</td>' + 
+      '<td>' + user.birthday.toLocaleDateString() + '</td>' + 
+      '<td>' + user.startTraining + '</td>';
+      tbody.appendChild(tr);
+      studensData.push(user);
+      console.log(studensData);
 }
 
 
 function setForm() {
-  event.preventDefault();
   const FORM = document.forms.form;
   let user = {
     sureName: FORM.elements.sureName.value,
@@ -32,11 +26,8 @@ function setForm() {
     birthday: new Date(FORM.elements.birthday.value),
     startTraining: FORM.elements.startTraining.value,
   }
-  
-  timeСalculation(user.birthday);
-
-  studensData.push(user);
+  return user;
 }
 
 const submit = document.querySelector('#form');
-submit.addEventListener('submit', setForm);
+submit.addEventListener('submit', addUserTable);
