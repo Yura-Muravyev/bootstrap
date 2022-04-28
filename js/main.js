6a5fb8612967aa8b5943fe43b20f4b78eb098582
Yuriy Muravyev <yura.muravyev@yandex.ru>
2022-04-28 16:58:47 +0300

    v3.3

--- js/main.js
+++ js/main.js
@@ -1,33 +1,83 @@
-const studensData = [];
-
-function addUserTable() {
-  event.preventDefault();
-  const user = setForm();
-  let tbody = document.querySelector('tbody');
-  let tr = document.createElement('tr');
-  tr.innerHTML =
-      '<td>' + user.sureName + ' ' + user.name + ' ' + user.middleName +'</td>' + 
-      '<td>' + user.faculty + '</td>' + 
-      '<td>' + user.birthday.toLocaleDateString() + '</td>' + 
-      '<td>' + user.startTraining + '</td>';
+(function () {
+  let studentsData = [];
+  
+  if (localStorage.getItem('student')) {
+    studentsData = JSON.parse(localStorage.getItem('student'));
+    studentsData.forEach(item => {
+      let tbody = document.querySelector('tbody');
+      let tr = document.createElement('tr');
+      tr.innerHTML =
+          '<td>' + item.sureName+ ' ' + item.name + ' ' + item.middleName +'</td>' + 
+          '<td>' + item.faculty + '</td>' + 
+          '<td>' + item.birthday + '</td>' + 
+          '<td>' + item.startTraining + '</td>';
       tbody.appendChild(tr);
-      studensData.push(user);
-      console.log(studensData);
-}
-
-
-function setForm() {
-  const FORM = document.forms.form;
-  let user = {
-    sureName: FORM.elements.sureName.value,
-    name: FORM.elements.name.value,
-    middleName: FORM.elements.middleName.value,
-    faculty: FORM.elements.faculty.value,
-    birthday: new Date(FORM.elements.birthday.value),
-    startTraining: FORM.elements.startTraining.value,
+    });
   }
-  return user;
-}
+  
+    
+  function getLS(data) {
+    localStorage.setItem('student', JSON.stringify(data));
+  }
 
-const submit = document.querySelector('#form');
-submit.addEventListener('submit', addUserTable);
+
+  function getForm() {
+    const FORM = document.forms.form;
+    let sureName = FORM.elements.sureName.value;
+    let name = FORM.elements.name.value;
+    let middleName = FORM.elements.middleName.value;
+    let startTraining = FORM.elements.startTraining.value
+    let faculty = FORM.elements.faculty.value;
+    
+    let age = ((new Date().getTime() - new Date(FORM.elements.birthday.valueAsDate)) / (24 * 3600 * 365.25 * 1000)) | 0;
+    let birthday = FORM.elements.birthday.valueAsDate.getDate() + '.' + FORM.elements.birthday.valueAsDate.getMonth()  + '.' + FORM.elements.birthday.valueAsDate.getFullYear();
+
+    return {
+      sureName,
+      name,
+      middleName,
+      startTraining,
+      faculty,
+      age,
+      birthday
+    }
+  }
+
+
+  function createStudentObj(data) {
+    const studentDate = getForm();
+    let user = {
+      sureName: studentDate.sureName,
+      name: studentDate.name,
+      middleName: studentDate.middleName,
+      faculty: studentDate.faculty,
+      age: studentDate.age,
+      birthday: studentDate.birthday,
+      startTraining: studentDate.startTraining,
+    }
+    data.push(user);
+    return user
+  }
+  
+  function addStudentsTable() { 
+    let user = createStudentObj();
+    let tbody = document.querySelector('tbody');
+    let tr = document.createElement('tr');
+    tr.innerHTML =
+        '<td>' + user.sureName+ ' ' + user.name + ' ' + user.middleName +'</td>' + 
+        '<td>' + user.faculty + '</td>' + 
+        '<td>' + user.birthday + ` ${user.age}` + '</td>' + 
+        '<td>' + user.startTraining + '</td>';
+    tbody.appendChild(tr);
+  }
+  
+  
+  const submit = document.querySelector('#form');
+  submit.addEventListener('submit', function (event) {
+    event.preventDefault();
+  
+    createStudentObj(studentsData);
+    getLS(studentsData);
+    addStudentsTable();
+  });
+})()
